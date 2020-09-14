1e1c59a6b69c57da9f259803bb472b7932beef31
Suyi <thonatos.yang@gmail.com>
2020-09-14 14:45:49 +0800

    feat: update script

--- packages/boilerplate-typescript-egg-umi/boilerplate/app/view/locale/zh-CN.ts
+++ packages/boilerplate-typescript-egg-umi/boilerplate/app/view/locale/zh-CN.ts
@@ -1,9 +1,11 @@
 export default {
   'site.title': '脚手架',
-  'site.index': '主页',
-  'menu.index': '主页',
+  
+  'site.index': '主页',  
   'site.info': '信息',
-  'menu.info': '信息',
   'site.login': '登录',
+
+  'menu.index': '主页',
+  'menu.info': '信息',  
   'menu.login': '登录',
 };
